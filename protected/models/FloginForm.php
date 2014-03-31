81b1131692dfd2e1fd277b414c86dfb5aaeabe65
github-zjh <xb_zjh@126.com>
2014-03-31 21:19:11 +0800

    update

---
+++ protected/models/FloginForm.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Front LoginForm class.
+ * LoginForm is the data structure for keeping
+ * user login form data. It is used by the 'login' action of 'SiteController'.
+ */
+class FloginForm extends CFormModel
+{
+	public $username;
+	public $password;
+	public $rememberMe;
+	public $verifyCode;
+	public $status;
+	public $groupid;
+
+	private $_identity;	
+
+	/**
+	 * Declares the validation rules.
+	 * The rules state that username and password are required,
+	 * and password needs to be authenticated.
+	 */
+	public function rules()
+	{
+		return array(
+			// username and password are required
+			array('username', 'required', 'message' => Yii::t('common','Username is required')),
+			array('password', 'required', 'message' => Yii::t('common','Password is required')),
+			// rememberMe needs to be a boolean
+			array('rememberMe', 'boolean'),
+			// password needs to be authenticated
+			array('password', 'authenticate'),
+			array('verifyCode', 'captcha', 'allowEmpty'=>!CCaptcha::checkRequirements()),
+		);
+	}
+
+	/**
+	 * Declares attribute labels.
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'rememberMe'=>'Remember me next time',
+		);
+	}
+
+	/**
+	 * Authenticates the password.
+	 * This is the 'authenticate' validator as declared in rules().
+	 */
+	public function authenticate()
+	{
+		if(!$this->hasErrors())
+		{
+			$tmpuser = User::model()->find('username=:username',array('username'=>$this->username));
+			$this->_identity=new UserIdentity($this->username,$this->password);					
+			if(!$this->_identity->authenticate())
+				$this->addError('password',Yii::t('common','Incorrect username or password.'));
+		}
+	}
+
+	/**
+	 * Logs in the user using the given username and password in the model.
+	 * @return boolean whether login is successful
+	 */
+	public function login()
+	{			
+		if($this->_identity===null)
+		{
+			$this->_identity=new UserIdentity($this->username,$this->password);
+			$this->_identity->authenticate();
+		}
+		if($this->_identity->errorCode===UserIdentity::ERROR_NONE)
+		{
+			$duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
+			Yii::app()->user->login($this->_identity,$duration);
+			return true;
+		}
+		else
+			return false;
+	}	
+	
+}
