e8025784eacd85239c2b0f974e48d25797a991d3
github-zjh <xb_zjh@126.com>
2014-05-22 22:30:02 +0800

    修改kindeditor图片管理

---
+++ protected/modules/admin/controllers/KindeditorController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * KindEditor图片管理
+ * 
+ * @author        zhao jinhan <326196998@qq.com>
+ * @copyright     Copyright (c) 2014-2015. All rights reserved.
+ * 
+ */
+class KindEditorController extends Backend
+{		
+	
+    /**
+	 * KindEditor管理
+	 *
+	 */
+    public function actionIndex ()
+    {  
+    	Yii::import('application.extensions.kindeditor.file_manager_json',true);     	
+    }
+   
+}
