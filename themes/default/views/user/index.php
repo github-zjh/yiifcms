d0238025680c766843a18986a4221e2d0d75eaa8
github-zjh <xb_zjh@126.com>
2014-08-11 22:33:02 +0800

    修改用户头像显示问题

--- themes/default/views/user/index.php
+++ themes/default/views/user/index.php
@@ -1,7 +1,7 @@
 	<div class="user">
 		<div class="user_left">
 			<div class="base_box clear">
-				<img class="fl" width="100" height="100" alt="<?php echo Yii::t('common','My Avatar');?>" src="<?php echo file_exists($profile->avatar)?$profile->avatar:$this->_stylePath.'/images/avatar-max-img.png';?>" />
+				<img class="fl" width="100" height="100" alt="<?php echo Yii::t('common','My Avatar');?>" src="<?php echo $profile->avatar?$profile->avatar:$this->_stylePath.'/images/avatar-max-img.png';?>" />
 				<ul class="base_info">
 					<li><label><?php echo Yii::t('model','username');?>：</label><span><?php echo $profile->username;?></span></li>
 					<li><label><?php echo Yii::t('model','email');?>：</label><span><?php echo $profile->email;?></span></li>
