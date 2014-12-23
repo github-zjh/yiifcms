61be1313144466fd86a45a92e044860eea20e3e8
github-zjh <xb_zjh@126.com>
2014-12-23 10:14:47 +0800

    修改yii框架获取客户端ip的方法

--- framework/web/CHttpRequest.php
+++ framework/web/CHttpRequest.php
@@ -675,7 +675,24 @@ class CHttpRequest extends CApplicationComponent
 	 */
 	public function getUserHost()
 	{
-		return isset($_SERVER['REMOTE_HOST'])?$_SERVER['REMOTE_HOST']:null;
+		//return isset($_SERVER['REMOTE_HOST'])?$_SERVER['REMOTE_HOST']:null;
+		static $ip = NULL;
+		if ( $ip !== NULL )
+			return $ip;
+		if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+			$arr = explode( ',', $_SERVER['HTTP_X_FORWARDED_FOR'] );
+			$pos = array_search( 'unknown', $arr );
+			if ( false !== $pos )
+				unset( $arr[$pos] );
+			$ip = trim( $arr[0] );
+		} elseif ( isset( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} elseif ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		// IP地址合法验证
+		$ip = ( false !== ip2long( $ip ) ) ? $ip : '0.0.0.0';
+		return $ip;
 	}
 
 	/**
